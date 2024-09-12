fed171c4bb1f77a310fcd410016c3710c5c4fc1d
Paige <paige.struthers@gmail.com>
2024-09-12 15:54:26 -0500

    equality examples

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-/**
- * Converting Temperatures
- * 
- * Celsius-to-Fahrenheit formula:
- * 
- *  F = C x 1.8 + 32
- */
-
-let celsius = 10;
-let fahrenheit = celsius * 1.8 + 32; /* convert celsius to fahrenheit */
-
-console.log(fahrenheit); /*print out result */
+let bool = '1' == 1
+console.log(bool) /* returns true */
+bool = '1' === 1
+console.log(bool) /* returns false */
