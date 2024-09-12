ede329189acef108d883985c4b8cbbd55b0f90a1
Paige <paige.struthers@gmail.com>
2024-09-12 16:44:05 -0500

    truthy vs falsy values

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let cash = 50
-let price = 40
-let isStoreOpen = false
-
-if (cash >= price && isStoreOpen === true) { /* evaluates to true && false */
-    console.log('print the receipt')
+if ("0") {
+    console.log('truthy value') /* prints */
 }
-else if (cash >= price || isStoreOpen === true) { /* evaluates to true || false */
-    console.log('you have enough cash, but store is closed') /* prints */
+else if (0) {
+    console.log('falsy value')
 }
