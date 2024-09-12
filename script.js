202eb7a94c166e74a9e15b9e05a60ea967395a43
Paige <paige.struthers@gmail.com>
2024-09-12 16:21:35 -0500

    conditional statements example

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let bool = '1' == 1
-console.log(bool) /* returns true */
-bool = '1' === 1
-console.log(bool) /* returns false */
+let cash = 35
+let price = 40
+let difference = cash - price
+
+if (cash > price) {
+    console.log(`you paid extra - here's ${difference} dollars in change`)
+}
+else if (cash === price) {
+    console.log("you paid the exact amount - have a nice day!")
+}
+else {
+    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+}
