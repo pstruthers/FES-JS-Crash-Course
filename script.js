d8fdf962563e85eea715b225d6b8f2c0dad296ac
Paige <paige.struthers@gmail.com>
2024-09-13 14:27:43 -0500

    for loop advanced

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-/* print numbers 1 - 100 */
-for (let i = 0; i < 100; i++) {
-    console.log(i + 1)
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> "Frontend Simplified"`)
+    }
+    else if (i % 3 === 0) {
+        console.log(`${i} -> "Frontend"`)
+    }
+    else if (i % 5 === 0) {
+        console.log(`${i} -> "Simplified"`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
 }
