caac0c6d7c34b2499d3bc6afc7adf24ac2efeab3
Paige <paige.struthers@gmail.com>
2024-09-13 13:50:24 -0500

    ternary operators

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-if ("0") {
-    console.log('truthy value') /* prints */
-}
-else if (0) {
-    console.log('falsy value')
-}
+let cash = 50
+let price = 40
+let isStoreOpen = true
+
+let str = isStoreOpen && cash >= price ? 'give receipt' : 'do not give receipt'
+console.log(str)
