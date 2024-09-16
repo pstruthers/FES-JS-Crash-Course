b01f6ba8928a266769edc0df6943701529b4d7eb
Paige <paige.struthers@gmail.com>
2024-09-16 15:58:13 -0500

    array of objects

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let user = {
-    username: 'Paige',
-    email: 'paige.struthers@gmail.com',
-    subscriptionStatus: 'VIP',
-    discordId: 'Paige#0001',
-    lessonsCompleted: [0, 1, 2],
-}
+let users = [
+    {
+        username: 'Paige',
+        email: 'paige.struthers@gmail.com',
+        subscriptionStatus: 'VIP',
+        discordID: 'Paige#0001',
+        lessonsCompleted: [0, 1]
+    },
+    {
+        username: 'David',
+        email: 'david@frontendsimplified.com',
+        subscriptionStatus: 'VIP',
+        discordID: 'David Bragg#0001',
+        lessonsCompleted: [0, 1, 2, 3, 4]
+    }
+]
 
-console.log(user.username) //prints Paige
-console.log(user.subscriptionStatus) //prints VIP
+console.log(users[0].username) //prints Paige
+console.log(users[1].discordID) //prints David Bragg#0001
