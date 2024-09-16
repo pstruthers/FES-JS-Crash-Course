63ca2f96277ba0a9f96464b937afbc81a3d9881f
Paige <paige.struthers@gmail.com>
2024-09-16 18:51:40 -0500

    innerHTML to change HTML of an element

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// First way of accessing an element
-console.log(document.querySelector('#title'))
-
-// Second way of accessing an element (only works if element has an ID)
-console.log(document.getElementById('title'))
+document.querySelector('#title').innerHTML = 'Frontend Simplified'
