d7c87f27d68df90c0adc27f8a741390913784ab4
Paige <paige.struthers@gmail.com>
2024-09-16 18:57:26 -0500

    change CSS using style

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Change HTML
 document.querySelector('#title').innerHTML += 'Frontend Simplified'
+
+// Change CSS
+document.querySelector('#title').style.fontSize = '14px'
+document.querySelector('#title').style.color = 'red'
