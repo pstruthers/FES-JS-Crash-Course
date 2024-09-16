449b0e797637ca5d89cdad569d928d5f497caff2
Paige <paige.struthers@gmail.com>
2024-09-16 14:39:21 -0500

    array push

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let arr = [20, 30, 40, 50, 100]
 
-// First element of array:
-console.log(arr[0])
+arr.push(200)
 
-// Last element of array:
-console.log(arr[arr.length - 1])
+console.log(arr) //prints [20, 30, 40, 50, 100, 200]
