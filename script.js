1f1c3cd8b72062949c30dcc5e431817eeacdf9df
Paige <paige.struthers@gmail.com>
2024-09-16 16:06:52 -0500

    login example using objects

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let users = [
     {
         username: 'Paige',
         email: 'paige.struthers@gmail.com',
+        password: 'test123',
         subscriptionStatus: 'VIP',
         discordID: 'Paige#0001',
         lessonsCompleted: [0, 1]
@@ -9,11 +10,26 @@ let users = [
     {
         username: 'David',
         email: 'david@frontendsimplified.com',
+        password: 'FES!',
         subscriptionStatus: 'VIP',
         discordID: 'David Bragg#0001',
         lessonsCompleted: [0, 1, 2, 3, 4]
     }
 ]
 
-console.log(users[0].username) //prints Paige
-console.log(users[1].discordID) //prints David Bragg#0001
+function login(email, password) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            console.log(users[i])
+            if (users[i].password === password) {
+                console.log('log the user in - the details are correct')
+            }
+            else {
+                console.log('login failed - the details are incorrect')
+            }
+        }
+    }
+    
+}
+
+login('paige.struthers@gmail.com', 'test123')
