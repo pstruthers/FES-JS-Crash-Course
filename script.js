6e6869000fb8bdf187b48d39d9576fb9bde58653
Paige <paige.struthers@gmail.com>
2024-09-16 19:12:50 -0500

    toggle dark mode

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Change HTML
-document.querySelector('#title').innerHTML += 'Frontend Simplified'
-
-// Change CSS
-document.querySelector('#title').style.fontSize = '14px'
-
 // JavaScript
-function changeTitleToRed() {
-    document.querySelector('#title').style.color = 'red'
+function toggleDarkMode() {
+    document.querySelector('body').classList.toggle("dark-theme")
 }
