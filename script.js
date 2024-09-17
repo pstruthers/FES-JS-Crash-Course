26cec53c321231033500f4acf35d7b33831fcc1e
Paige <paige.struthers@gmail.com>
2024-09-17 13:55:44 -0500

    creating a promise

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-const emailRef = document.querySelector(".email");
+// Creating a new promise
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    resolve("VIP");
+  });
+}
 
-// Method 1: Then
- fetch("https://jsonplaceholder.typicode.com/users/1") // fetch data from backend API
-   .then((response) => { // use .then() to unlock data
-     return response.json(); // return the response from fetching; add .json() to response to make it compatible with frontend; when returning response, whole thing becomes a promise
-   })
-   .then((data) => { // .then() to unlock promise as a result from returning the response
-     console.log(data);
-     emailRef.innerHTML = data.email // display the user's email on the website
-   });
+// Accessing promise using .then()
+getSubscriptionStatus().then((response) => console.log(response)); // no need to use .json() since it's not coming from backend
 
-// Method 2: Async/Await
-async function main() { // must put await inside of an async function
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/1"); // add await before a promise and store it in a variable
-  const data = await response.json(); // add await before a promise and store it in a variable
-  console.log(data);
-  emailRef.innerHTML = data.email; // display user's email on the website
+// Accessing promise using async/await
+async function main() {
+  console.log(await getSubscriptionStatus());
 }
 
 main();
