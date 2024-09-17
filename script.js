7cd598aca011dd5815a9d7339a5dbcdc4f79f475
Paige <paige.struthers@gmail.com>
2024-09-17 14:23:22 -0500

    example exercise to show video based on subscription status

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
+// JAVASCRIPT
+
 const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video")
 
 // Creating a new promise
 function getSubscriptionStatus() {
   return new Promise((resolve, reject) => {
-    resolve("VIP");
+    resolve("VIadfP");
   });
+}
+
+function getVideo(subscriptionStatus) {
+    return new Promise((resolve, reject) => {
+        if (subscriptionStatus === "VIP") {
+            resolve("show video")
+        } else if (subscriptionStatus === "FREE") {
+            resolve("show trailer")
+        } else {
+            reject("no video")
+        }
+    })
 }
 
 // Accessing promise using async/await
 async function main() {
   const status = await getSubscriptionStatus();
   statusRef.innerHTML = status;
+  // use a try-catch to display error message without throwing an actual error
+  try {
+      console.log(await getVideo(status))
+  }
+  catch(e) {
+    console.log(e)
+    videoRef.innerHTML = e
+  }
 }
 
 main();
